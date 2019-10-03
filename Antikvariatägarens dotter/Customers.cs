a1f253c8fa7f6e3ab5634573d21bb65609d7f460
Filip-Ericsson <filip.ericsson@elev.ga.ntig.se>
2019-10-03 13:39:28 +0200

    Update Customers.cs

--- Antikvariatägarens dotter/Customers.cs
+++ Antikvariatägarens dotter/Customers.cs
@@ -8,21 +8,21 @@ namespace Antikvariatägarens_dotter
 {
     class Customers
     {
-        private int money;
-        public string name;
-        private string categoryIntrest;
-        private string booksAlreadyOwned;
+        private int money; //skapade denna int variabel eftersom en besökare måste ha pengar att köpa böker med
+        public string name; //Vem vill inte ha ett namn
+        private string categoryIntrest; //Olika besökare kan ha olika intressen vilket gör att de söker olika kategorier av böcker
+        private string booksAlreadyOwned; // Om en kund redan äger en bok kommer hen inte att köpa den
         Random gen = new Random();
 
-        private List<int> customer = new List<int>();
+        private List<int> customer = new List<int>(); //En lista på hur många kunder som finns i butiken 
 
 
         public Customers()
         {
-
+            //Här skulle namn, pängar, categoryIntrest och bookAlreadyOwned slumpas
         }
 
-        public void AddCustomers(int addCustomers)
+        public void AddCustomers(int addCustomers) // denna metod taremot användar input och adderar mängden besökare som användaren önskar 
         {
             customer.Add(addCustomers);
 
